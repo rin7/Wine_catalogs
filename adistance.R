1238d3b5f12c4c8d753cf8e7f563ca923ab686c9
Arin Sa <rinsa@ucdavis.edu>
2018-06-08 22:41:54 -0700

    adist

---
+++ adistance.R
@@ -0,0 +1,10 @@
+truth <- read.csv("C:/Users/ARIN/Google Drive/DaViS/Quarters/Spring 18/STA 160 - Stat data science project/project/training set/dft_0069t.csv")
+sample <- read.csv("C:/Users/ARIN/Google Drive/DaViS/Quarters/Spring 18/STA 160 - Stat data science project/project/training set/df_0069.csv")
+  
+output <- read.table("")
+
+a <- "454 Anjou Rose 1966,Nicolas."
+b <- "454 Anjou Rose 1966, Nicolas"
+adist()
+adist(truth,sample, ignore.case = FALSE)
+head(sample)
