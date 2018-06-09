915ab634c32963ee886909d552ca5de21095158a
Arin Sa <rinsa@ucdavis.edu>
2018-06-09 00:16:33 -0700

    total accuracy

--- adistance.R
+++ adistance.R
@@ -1,11 +1,21 @@
-truth <- read.csv("C:/Users/ARIN/Google Drive/DaViS/Quarters/Spring 18/STA 160 - Stat data science project/project/training set/dft_0069t.csv")
+truth <- read.csv("C:/Users/ARIN/Google Drive/DaViS/Quarters/Spring 18/STA 160 - Stat data science project/project/training set/df_0069t.csv")
 sample <- read.csv("C:/Users/ARIN/Google Drive/DaViS/Quarters/Spring 18/STA 160 - Stat data science project/project/training set/df_0069.csv")
-  
-output <- read.table("")
 
-a <- "454 Anjou Rose 1966,Nicolas."
-b <- "454 Anjou Rose 1966, Nicolas"
-adist()
-adist(truth,sample, ignore.case = FALSE)
-nrow(truth)
+#convert the column of name to charachter type
+truth[, 2] <- sapply(truth[, 2], as.character)
+sample[, 2] <- sapply(sample[, 2], as.character)
+
+accuracy <- adist(truth, sample, ignore.case = F, counts = T, fixed = T)
+diag(accuracy)
+
+class(accuracy)
+dim(accuracy)
+
+sapply(truth, class)
 nrow(sample)
+
+
+## Cf. the examples for agrep:
+adist("lasy", "1 lazy 2")
+## For a "partial approximate match" (as used for agrep):
+adist("lasy", "1 lazy 2", partial = TRUE)
